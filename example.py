a58cf3552b227364d1a29d4b9aee17119d56457d
Robin Meis <robin@meis.space>
2017-11-28 22:46:56 +0100

    Basic example

---
+++ example.py
@@ -0,0 +1,18 @@
+from ttn_mqtt import ttn_mqtt
+from ttn_mqtt import ttn_constants
+from ttn_mqtt import ttn_device
+
+def test_node_callback(payload):
+    print("The callback was fired")
+    print(payload)
+
+ttn = ttn_mqtt.ttn_mqtt(ttn_constants.EU, "Application ID", "Application access key") #Open connection
+
+test_node = ttn_device.device("u23-node-smartnoob") #Register node
+test_node.set_uplink_callback(test_node_callback)
+ttn.register_device(test_node)
+
+ttn.connect() #Connect to TTN
+
+input("Press button to stop")
+ttn.disconnect()
