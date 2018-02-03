15f08582f22603370c68b66be8cc585feb9b1587
Robin Meis <robin@meis.space>
2018-02-03 12:35:14 +0100

    Added details for parameters

--- example.py
+++ example.py
@@ -3,13 +3,12 @@ from ttn_mqtt import ttn_constants
 from ttn_mqtt import ttn_device
 
 def test_node_callback(payload):
-    print("The callback was fired")
     print(payload)
 
-ttn = ttn_mqtt.ttn_mqtt(ttn_constants.EU, "Application ID", "Application access key") #Open connection
+ttn = ttn_mqtt.ttn_mqtt(ttn_constants.EU, "Application ID (name, not EUI)", "Application access key (ttn-account-v2...)") #Open connection
 ttn.connect() #Connect to TTN
 
-test_node = ttn_device.device("node-id") #Register node
+test_node = ttn_device.device("node-id (name, not EUI)") #Register node
 test_node.set_uplink_callback(test_node_callback)
 ttn.register_device(test_node)
 
